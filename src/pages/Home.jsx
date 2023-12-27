65f4a7d0a76d054076525bb2132864073b158ed3
Giorgi Rapava <grapava00@gmail.com>
2023-12-28 03:14:51 +0400

    remove a tag

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
 import cover from ".././assets/cover.png";
 import blogCover from ".././assets/blog-cover.png";
 import showFullBlogIcon from ".././assets/show-full-blog.svg";
 import Header from "../components/Header";
+import { Link } from "react-router-dom";
 import "./home.css";
 
 function Home() {
@@ -56,150 +56,10 @@ function Home() {
                 6 თვის შემდეგ ყველის ბრმა დეგუსტაციის დროც დადგა. მაქსიმალური
                 სიზუსტისთვის, ეს პროცესი...
               </p>
-              <a href='full-article.html'>
+              <Link to='/blog'>
                 სრულად ნახვა
                 <img src={showFullBlogIcon} alt='show full blog icon' />
-              </a>
-            </div>
-          </article>
-
-          <article className='blog'>
-            <img src={blogCover} alt='Blog Cover' className='blog-cover' />
-            <div className='grid-container'>
-              <h3>ნია გოგსაძე</h3>
-              <time dateTime='2023-11-02'>02.11.2023</time>
-              <h2>EOMM-ის მრჩეველთა საბჭოს ნინო ეგაძე შეუერთდა</h2>
-              <ul className='flex-container'>
-                <li className='category' data-item-type='accent-1'>
-                  მარკეტი
-                </li>
-                <li className='category' data-item-type='accent-2'>
-                  აპლიკაცია
-                </li>
-                <li className='category' data-item-type='accent-3'>
-                  ხელოვნური ინტელექტი
-                </li>
-              </ul>
-              <p className='description'>
-                6 თვის შემდეგ ყველის ბრმა დეგუსტაციის დროც დადგა. მაქსიმალური
-                სიზუსტისთვის, ეს პროცესი...
-              </p>
-              <a href='full-article.html'>
-                სრულად ნახვა
-                <img src={showFullBlogIcon} alt='show full blog icon' />
-              </a>
-            </div>
-          </article>
-
-          <article className='blog'>
-            <img src={blogCover} alt='Blog Cover' className='blog-cover' />
-            <div className='grid-container'>
-              <h3>ნია გოგსაძე</h3>
-              <time dateTime='2023-11-02'>02.11.2023</time>
-              <h2>EOMM-ის მრჩეველთა საბჭოს ნინო ეგაძე შეუერთდა</h2>
-              <ul className='flex-container'>
-                <li className='category' data-item-type='accent-1'>
-                  მარკეტი
-                </li>
-                <li className='category' data-item-type='accent-2'>
-                  აპლიკაცია
-                </li>
-                <li className='category' data-item-type='accent-3'>
-                  ხელოვნური ინტელექტი
-                </li>
-              </ul>
-              <p className='description'>
-                6 თვის შემდეგ ყველის ბრმა დეგუსტაციის დროც დადგა. მაქსიმალური
-                სიზუსტისთვის, ეს პროცესი...
-              </p>
-              <a href='full-article.html'>
-                სრულად ნახვა
-                <img src={showFullBlogIcon} alt='show full blog icon' />
-              </a>
-            </div>
-          </article>
-
-          <article className='blog'>
-            <img src={blogCover} alt='Blog Cover' className='blog-cover' />
-            <div className='grid-container'>
-              <h3>ნია გოგსაძე</h3>
-              <time dateTime='2023-11-02'>02.11.2023</time>
-              <h2>EOMM-ის მრჩეველთა საბჭოს ნინო ეგაძე შეუერთდა</h2>
-              <ul className='flex-container'>
-                <li className='category' data-item-type='accent-1'>
-                  მარკეტი
-                </li>
-                <li className='category' data-item-type='accent-2'>
-                  აპლიკაცია
-                </li>
-                <li className='category' data-item-type='accent-3'>
-                  ხელოვნური ინტელექტი
-                </li>
-              </ul>
-              <p className='description'>
-                6 თვის შემდეგ ყველის ბრმა დეგუსტაციის დროც დადგა. მაქსიმალური
-                სიზუსტისთვის, ეს პროცესი...
-              </p>
-              <a href='full-article.html'>
-                სრულად ნახვა
-                <img src={showFullBlogIcon} alt='show full blog icon' />
-              </a>
-            </div>
-          </article>
-
-          <article className='blog'>
-            <img src={blogCover} alt='Blog Cover' className='blog-cover' />
-            <div className='grid-container'>
-              <h3>ნია გოგსაძე</h3>
-              <time dateTime='2023-11-02'>02.11.2023</time>
-              <h2>EOMM-ის მრჩეველთა საბჭოს ნინო ეგაძე შეუერთდა</h2>
-              <ul className='flex-container'>
-                <li className='category' data-item-type='accent-1'>
-                  მარკეტი
-                </li>
-                <li className='category' data-item-type='accent-2'>
-                  აპლიკაცია
-                </li>
-                <li className='category' data-item-type='accent-3'>
-                  ხელოვნური ინტელექტი
-                </li>
-              </ul>
-              <p className='description'>
-                6 თვის შემდეგ ყველის ბრმა დეგუსტაციის დროც დადგა. მაქსიმალური
-                სიზუსტისთვის, ეს პროცესი...
-              </p>
-              <a href='full-article.html'>
-                სრულად ნახვა
-                <img src={showFullBlogIcon} alt='show full blog icon' />
-              </a>
-            </div>
-          </article>
-
-          <article className='blog'>
-            <img src={blogCover} alt='Blog Cover' className='blog-cover' />
-            <div className='grid-container'>
-              <h3>ნია გოგსაძე</h3>
-              <time dateTime='2023-11-02'>02.11.2023</time>
-              <h2>EOMM-ის მრჩეველთა საბჭოს ნინო ეგაძე შეუერთდა</h2>
-              <ul className='flex-container'>
-                <li className='category' data-item-type='accent-1'>
-                  მარკეტი
-                </li>
-                <li className='category' data-item-type='accent-2'>
-                  აპლიკაცია
-                </li>
-                <li className='category' data-item-type='accent-3'>
-                  ხელოვნური ინტელექტი
-                </li>
-              </ul>
-              <p className='description'>
-                6 თვის შემდეგ ყველის ბრმა დეგუსტაციის დროც დადგა. მაქსიმალური
-                სიზუსტისთვის, ეს პროცესი...
-              </p>
-              <a href='full-article.html'>
-                სრულად ნახვა
-                <img src={showFullBlogIcon} alt='show full blog icon' />
-              </a>
+              </Link>
             </div>
           </article>
         </div>
