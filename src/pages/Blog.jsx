4604260e24c6249323c2bd3e767c66eddaeea48f
Giorgi Rapava <grapava00@gmail.com>
2023-12-28 03:21:03 +0400

    remove index.html

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,6 +5,7 @@ import arrowGray from "../assets/Arrow-gray.svg";
 import arrowBlue from "../assets/Arrow-blue.svg";
 import blogCover from "../assets/blog-cover.png";
 import showFullBlogIcon from "../assets/show-full-blog.svg";
+import { Link } from "react-router-dom";
 import "./blog.css";
 
 function Blog() {
@@ -101,91 +102,10 @@ function Blog() {
                 6 თვის შემდეგ ყველის ბრმა დეგუსტაციის დროც დადგა. მაქსიმალური
                 სიზუსტისთვის, ეს პროცესი...
               </p>
-              <a href='full-article.html'>
+              <Link to='/blog'>
                 სრულად ნახვა
                 <img src={showFullBlogIcon} alt='show full blog icon' />
-              </a>
-            </div>
-          </article>
-          <article className='blog'>
-            <img src={blogCover} alt='Blog Cover' className='blog-cover' />
-            <div className='grid-container'>
-              <h3>ნია გოგსაძე</h3>
-              <time dateTime='2023-11-02'>02.11.2023</time>
-              <h2>EOMM-ის მრჩეველთა საბჭოს ნინო ეგაძე შეუერთდა</h2>
-              <ul className='flex-container'>
-                <li className='category' data-item-type='accent-1'>
-                  მარკეტი
-                </li>
-                <li className='category' data-item-type='accent-2'>
-                  აპლიკაცია
-                </li>
-                <li className='category' data-item-type='accent-3'>
-                  ხელოვნური ინტელექტი
-                </li>
-              </ul>
-              <p className='description'>
-                6 თვის შემდეგ ყველის ბრმა დეგუსტაციის დროც დადგა. მაქსიმალური
-                სიზუსტისთვის, ეს პროცესი...
-              </p>
-              <a href='full-article.html'>
-                სრულად ნახვა
-                <img src={showFullBlogIcon} alt='show full blog icon' />
-              </a>
-            </div>
-          </article>
-          <article className='blog'>
-            <img src={blogCover} alt='Blog Cover' className='blog-cover' />
-            <div className='grid-container'>
-              <h3>ნია გოგსაძე</h3>
-              <time dateTime='2023-11-02'>02.11.2023</time>
-              <h2>EOMM-ის მრჩეველთა საბჭოს ნინო ეგაძე შეუერთდა</h2>
-              <ul className='flex-container'>
-                <li className='category' data-item-type='accent-1'>
-                  მარკეტი
-                </li>
-                <li className='category' data-item-type='accent-2'>
-                  აპლიკაცია
-                </li>
-                <li className='category' data-item-type='accent-3'>
-                  ხელოვნური ინტელექტი
-                </li>
-              </ul>
-              <p className='description'>
-                6 თვის შემდეგ ყველის ბრმა დეგუსტაციის დროც დადგა. მაქსიმალური
-                სიზუსტისთვის, ეს პროცესი...
-              </p>
-              <a href='full-article.html'>
-                სრულად ნახვა
-                <img src={showFullBlogIcon} alt='show full blog icon' />
-              </a>
-            </div>
-          </article>
-          <article className='blog'>
-            <img src={blogCover} alt='Blog Cover' className='blog-cover' />
-            <div className='grid-container'>
-              <h3>ნია გოგსაძე</h3>
-              <time dateTime='2023-11-02'>02.11.2023</time>
-              <h2>EOMM-ის მრჩეველთა საბჭოს ნინო ეგაძე შეუერთდა</h2>
-              <ul className='flex-container'>
-                <li className='category' data-item-type='accent-1'>
-                  მარკეტი
-                </li>
-                <li className='category' data-item-type='accent-2'>
-                  აპლიკაცია
-                </li>
-                <li className='category' data-item-type='accent-3'>
-                  ხელოვნური ინტელექტი
-                </li>
-              </ul>
-              <p className='description'>
-                6 თვის შემდეგ ყველის ბრმა დეგუსტაციის დროც დადგა. მაქსიმალური
-                სიზუსტისთვის, ეს პროცესი...
-              </p>
-              <a href='full-article.html'>
-                სრულად ნახვა
-                <img src={showFullBlogIcon} alt='show full blog icon' />
-              </a>
+              </Link>
             </div>
           </article>
         </div>
