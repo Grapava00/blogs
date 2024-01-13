22e2a45c9c5d64b41b328333e53694bf3fc6e199
Giorgi Rapava <grapava00@gmail.com>
2024-01-13 15:52:56 +0400

    update goBackArrow comp styles

---
+++ src/components/go-back/goBack.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const GoBackArrow = styled.div`
+  position: absolute;
+  top: 60px;
+  left: 15px;
+
+  @media (min-width: 1287px) {
+    top: 129px;
+    left: 76px;
+  }
+`;
