c39363ae7bd74326e6ca75a94d29e318886629fa
Giorgi Rapava <grapava00@gmail.com>
2024-01-13 12:57:45 +0400

    update NavLink styles

---
+++ src/components/nav-link/navLink.styles.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const StyledLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+
+  color: #5d37f3;
+  font-size: 14px;
+  font-weight: 500;
+`;
