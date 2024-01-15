8fee1e41121c2895f2324c92bb398c759b6f9f90
Giorgi Rapava <grapava00@gmail.com>
2024-01-15 18:57:12 +0400

    update Header comp styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import ModalLogin from "../modals/login/LoginModal";
+import PropTypes from "prop-types";
+import { useState, useContext } from "react";
+import NavLink from "../nav-link/NavLink";
+import LoginModal from "../modals/login/LoginModal";
 import logoRedberry from "../../assets/logo.png";
-import { UseAppData } from "../../context/ContextProvider";
+import { AuthContext } from "../../context/AuthContext";
+import Button from "../button/Button";
+import { StyledHeader } from "./header.styles";
 
-function Header() {
+function Header({ display }) {
   const [showModal, setShowModal] = useState(false);
-  const { authenticated } = UseAppData();
+  const { isAuthenticated } = useContext(AuthContext);
 
   const handleShowModal = (condition) => {
     setShowModal(condition);
@@ -14,23 +17,27 @@ function Header() {
 
   return (
     <>
-      <header>
-        <Link to='/'>
-          <img src={logoRedberry} alt='redberry logo' className='logo' />
-        </Link>
-        {!authenticated ? (
-          <button className='login-button' onClick={() => setShowModal(true)}>
-            შესვლა
-          </button>
+      <StyledHeader
+        style={display === "none" ? { justifyContent: "center" } : {}}
+      >
+        <NavLink to='/'>
+          <img src={logoRedberry} alt='redberry logo' />
+        </NavLink>
+        {!isAuthenticated ? (
+          <Button onClick={() => setShowModal(true)}>შესვლა</Button>
         ) : (
-          <Link to='/create-blog'>
-            <button className='login-button'>ბლოგის დამატება</button>
-          </Link>
+          <NavLink to='/create-blog'>
+            <Button display={display}>ბლოგის დამატება</Button>
+          </NavLink>
         )}
-        <ModalLogin handleShowModal={handleShowModal} showModal={showModal} />
-      </header>
+        <LoginModal handleShowModal={handleShowModal} showModal={showModal} />
+      </StyledHeader>
     </>
   );
 }
+
+Header.propTypes = {
+  display: PropTypes.string,
+};
 
 export default Header;
